172a073d233cdaabbcac03b5a86531d4315e3d32
denis <denis.lazau@hotmail.com>
2024-11-12 08:23:30 +0200

    .

--- Biblioteca/obj/Debug/net6.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net6.0/Biblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48067ee2780e40d84f9175fa00ca0a58d4f73bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
